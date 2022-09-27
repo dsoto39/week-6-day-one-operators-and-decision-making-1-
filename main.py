4fd028169ff7a568f65ab40fb3d310e2d1308475
Diego Soto <113551815+dsoto39@users.noreply.github.com>
2022-09-27 19:43:23 +0000

    How to remove item

--- main.py
+++ main.py
@@ -17,11 +17,14 @@ current = 1.11
 
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
-
+seconds.remove(1.45)
+print(seconds)
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
+seconds.remove(1.02)
+seconds.remove(1.11)
+print(seconds)
 
 ################################comparison operators#########################
 #remember....
